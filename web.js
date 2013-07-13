b85cf4912519c9bccb0e88ab819e4537d0ad2297
stefanRitter <stefan@stefanritter.com>
2013-07-13 13:54:51 +0000

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync('index.html');
+var str = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
